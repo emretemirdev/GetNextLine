8aae4de695803bfb215c8086e466fd522c101b9c
Emre Temir <emre.temir@st.atlas.edu.tr>
2023-07-27 02:00:48 +0300

    initt

--- test.c
+++ test.c
@@ -1,20 +1,19 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+
  int main()
  {
     int fd;
     char buf[256];
     int char_reads;
 
-    fd = open("file.txt", O_RDWR | O_CREAT);
+    fd = open("file.txt", O_RDWR | O_CREAT, 0777);
 
     while ((char_reads = read(fd, buf, 1)))
     {
         buf[char_reads] = '\0';
-        usleep(150000);
         printf("buf -> %s\n", buf);
     }
-    
  }
 
