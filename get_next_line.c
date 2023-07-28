91d64df76a64b78745056d5bfcadf874632139d0
Aliemre44 <emre.temir@st.atlas.edu.tr>
2023-07-28 13:11:27 +0300

    update func

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,27 @@
 /*   By: emtemir <emtemir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 12:59:22 by emtemir           #+#    #+#             */
-/*   Updated: 2023/07/28 12:59:26 by emtemir          ###   ########.fr       */
+/*   Updated: 2023/07/28 13:10:56 by emtemir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char  *get_next_line(int fd)
+{
+    static char *str = NULL;
+    char        *buf;
+    int         char_reads;
+    int         i;
+
+    if (!str)
+        str = malloc(sizeof(char) * 256);
+    buf = malloc(sizeof(char) * 256);
+    i = 0;
+    while ((char_reads = read(fd, buf, 1)))
+    {
+        buf[char_reads] = '\0';
+        str[i] = buf[0];
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}
