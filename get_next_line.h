2e0401e65508afb6c145cfa2db658bd453f0db99
Emre Temir <102259373+emretemirdev@users.noreply.github.com>
2023-08-02 19:53:10 +0300

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -3,38 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emtemir <emtemir@student.42.fr>            +#+  +:+       +#+        */
+/*   By: emre <emre@student.1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/28 19:10:18 by emtemir           #+#    #+#             */
-/*   Updated: 2023/08/01 10:12:07 by emtemir          ###   ########.fr       */
+/*   Created: 2023/08/02 16:01:52 by emre              #+#    #+#             */
+/*   Updated: 2023/08/02 17:34:05 by emre             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 123
+#  define BUFFER_SIZE 5
 # endif
 
-# include <fcntl.h>
-# include <stdlib.h>
 # include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
-typedef struct s_list
-{
-	char			*str_buf;
-	struct s_list	*next;
-}				t_list;
-
-int		found_newline(t_list *list);
-t_list	*find_last_node(t_list *list);
-char	*get_line(t_list *list);
-void	copy_str(t_list *list, char *str);
-int		len_to_newline(t_list *list);
-void	polish_list(t_list **list);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
 char	*get_next_line(int fd);
-void	dealloc(t_list **list, t_list *clean_node, char *buf);
-void	create_list(t_list **list, int fd);
 
 #endif
