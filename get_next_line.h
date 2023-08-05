f47614c713d8d1d02f4ab643a5afc2f735053270
Aliemre44 <emre.temir@st.atlas.edu.tr>
2023-08-05 18:57:29 +0300

    initial commit

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: emtemir <emtemir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 16:01:52 by emre              #+#    #+#             */
-/*   Updated: 2023/08/05 17:19:02 by emtemir          ###   ########.fr       */
+/*   Updated: 2023/08/05 17:57:08 by emtemir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 15
@@ -25,5 +23,9 @@
 int		find_newline_index(char *buff);
 size_t	ft_strlen(char *str);
 char	*get_next_line(int fd);
+char	*get_remain_str(char *str, int nl_index, int i);
+char	*get_fline(char *str);
+char	*gnl_helper(char **str);
+char	*merge(char *str, char *buffer, int i, int j);
 
 #endif
